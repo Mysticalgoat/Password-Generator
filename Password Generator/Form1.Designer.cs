2ea73d27c5d2c9e539d30e58a600998901284fd2
Mysticalgoat <62557562+Mysticalgoat@users.noreply.github.com>
2021-05-19 09:36:30 +0100

    Add project files.

---
+++ Password Generator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_generate_pass = new System.Windows.Forms.Button();
+            this.chkbox_special_Char = new System.Windows.Forms.CheckBox();
+            this.pass_Length = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pass_Box = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pass_Length)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_generate_pass
+            // 
+            this.btn_generate_pass.Location = new System.Drawing.Point(12, 264);
+            this.btn_generate_pass.Name = "btn_generate_pass";
+            this.btn_generate_pass.Size = new System.Drawing.Size(96, 43);
+            this.btn_generate_pass.TabIndex = 4;
+            this.btn_generate_pass.Text = "Generate";
+            this.btn_generate_pass.UseVisualStyleBackColor = true;
+            this.btn_generate_pass.Click += new System.EventHandler(this.btn_generate_pass_Click);
+            // 
+            // chkbox_special_Char
+            // 
+            this.chkbox_special_Char.AutoSize = true;
+            this.chkbox_special_Char.Location = new System.Drawing.Point(12, 51);
+            this.chkbox_special_Char.Name = "chkbox_special_Char";
+            this.chkbox_special_Char.Size = new System.Drawing.Size(157, 21);
+            this.chkbox_special_Char.TabIndex = 2;
+            this.chkbox_special_Char.Text = "Special Characters?";
+            this.chkbox_special_Char.UseVisualStyleBackColor = true;
+            // 
+            // pass_Length
+            // 
+            this.pass_Length.Location = new System.Drawing.Point(139, 12);
+            this.pass_Length.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.pass_Length.Name = "pass_Length";
+            this.pass_Length.Size = new System.Drawing.Size(133, 22);
+            this.pass_Length.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password Length:";
+            // 
+            // pass_Box
+            // 
+            this.pass_Box.Location = new System.Drawing.Point(12, 78);
+            this.pass_Box.Name = "pass_Box";
+            this.pass_Box.Size = new System.Drawing.Size(293, 180);
+            this.pass_Box.TabIndex = 3;
+            this.pass_Box.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(317, 313);
+            this.Controls.Add(this.pass_Box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pass_Length);
+            this.Controls.Add(this.chkbox_special_Char);
+            this.Controls.Add(this.btn_generate_pass);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pass_Length)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generate_pass;
+        private System.Windows.Forms.CheckBox chkbox_special_Char;
+        private System.Windows.Forms.NumericUpDown pass_Length;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox pass_Box;
+    }
+}
+
---
+++ Password Generator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_generate_pass_Click(object sender, EventArgs e)
+        {
+            string[] lowerCase = { "a", "b", "c", "d", "e", "f", "g", "h", "j", "i", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "k", "y", "z" };
+            string[] upperCase = { "A", "B", "C", "D", "E", "F", "G", "H", "J", "I", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] specialChar = { "!", "#", "£", "@", "*", "^", "(", ")", ".", "+" };
+
+            int length = Convert.ToInt32(pass_Length.Value);
+            string Password = "";
+            Random rand = new Random();
+            //if special wanted
+            if (chkbox_special_Char.Checked)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    int selectlist = rand.Next(1, 5);
+                    //lower
+                    if (selectlist == 1)
+                    {
+                        int selectValue = rand.Next(0, 25);
+                        string nextValue = lowerCase[selectValue];
+                        Password += nextValue;
+                    }
+                    //upper
+                    if (selectlist == 2)
+                    {
+                        int selectValue = rand.Next(0, 25);
+                        string nextValue = upperCase[selectValue];
+                        Password += nextValue;
+                    }
+                    //number
+                    if (selectlist == 3)
+                    {
+                        int selectValue = rand.Next(0, 9);
+                        string nextValue = numbers[selectValue].ToString();
+                        Password += nextValue;
+                    }
+                    //special
+                    if (selectlist == 4)
+                    {
+                        int selectValue = rand.Next(0, 9);
+                        string nextValue = specialChar[selectValue].ToString();
+                        Password += nextValue;
+                    }
+                }// end of for
+
+            }//end of if special
+             //if no special
+            if (!chkbox_special_Char.Checked)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    int selectlist = rand.Next(1, 4);
+                    //lower
+                    if (selectlist == 1)
+                    {
+                        int selectValue = rand.Next(0, 25);
+                        string nextValue = lowerCase[selectValue];
+                        Password += nextValue;
+                    }
+                    //upper
+                    if (selectlist == 2)
+                    {
+                        int selectValue = rand.Next(0, 25);
+                        string nextValue = upperCase[selectValue];
+                        Password += nextValue;
+                    }
+                    //number
+                    if (selectlist == 3)
+                    {
+                        int selectValue = rand.Next(0, 9);
+                        string nextValue = numbers[selectValue].ToString();
+                        Password += nextValue;
+                    }
+                }
+            }//end of if not special
+            pass_Box.Clear();
+            pass_Box.Text = Password;
+
+
+        }
+    }
+}
