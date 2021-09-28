e118509d2eb6e5a3b22d47b920a85862ab2018d0
Mysticalgoat <62557562+Mysticalgoat@users.noreply.github.com>
2021-09-28 20:43:09 +0100

    refactored logic from IF statements to switch statement (reduced total code and easier to read) removed need for number lust

--- Password Generator/Form1.cs
+++ Password Generator/Form1.cs
@@ -23,82 +23,71 @@ namespace Password_Generator
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "k", "y", "z" };
             string[] upperCase = { "A", "B", "C", "D", "E", "F", "G", "H", "J", "I", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             string[] specialChar = { "!", "#", "£", "@", "*", "^", "(", ")", ".", "+" };
 
-            int length = Convert.ToInt32(pass_Length.Value);
+            int length = Convert.ToInt32(pass_Length.Value); //Gets required length of password
             string Password = "";
-            Random rand = new Random();
+            Random rand = new Random(); //instanciates a new rand object
+
             //if special wanted
             if (chkbox_special_Char.Checked)
             {
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < length; i++) // loops length of password
                 {
-                    int selectlist = rand.Next(1, 5);
-                    //lower
-                    if (selectlist == 1)
+                    int selectlist = rand.Next(0, 4);
+                    switch (selectlist)
                     {
-                        int selectValue = rand.Next(0, 25);
-                        string nextValue = lowerCase[selectValue];
-                        Password += nextValue;
-                    }
-                    //upper
-                    if (selectlist == 2)
-                    {
-                        int selectValue = rand.Next(0, 25);
-                        string nextValue = upperCase[selectValue];
-                        Password += nextValue;
-                    }
-                    //number
-                    if (selectlist == 3)
-                    {
-                        int selectValue = rand.Next(0, 9);
-                        string nextValue = numbers[selectValue].ToString();
-                        Password += nextValue;
-                    }
-                    //special
-                    if (selectlist == 4)
-                    {
-                        int selectValue = rand.Next(0, 9);
-                        string nextValue = specialChar[selectValue].ToString();
-                        Password += nextValue;
-                    }
-                }// end of for
+                        //lower
+                        case 0:
+                            Password += lowerCase[rand.Next(0, 26)].ToString();
+                            break;
 
-            }//end of if special
-             //if no special
-            if (!chkbox_special_Char.Checked)
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    int selectlist = rand.Next(1, 4);
-                    //lower
-                    if (selectlist == 1)
-                    {
-                        int selectValue = rand.Next(0, 25);
-                        string nextValue = lowerCase[selectValue];
-                        Password += nextValue;
-                    }
-                    //upper
-                    if (selectlist == 2)
-                    {
-                        int selectValue = rand.Next(0, 25);
-                        string nextValue = upperCase[selectValue];
-                        Password += nextValue;
-                    }
-                    //number
-                    if (selectlist == 3)
-                    {
-                        int selectValue = rand.Next(0, 9);
-                        string nextValue = numbers[selectValue].ToString();
-                        Password += nextValue;
+                        //upper
+                        case 1:
+                            Password += upperCase[rand.Next(0, 26)].ToString();
+                            break;
+
+                        //Number
+                        case 2:
+                            Password += rand.Next(0, 10).ToString().ToString();
+                            break;
+
+                        //special
+                        case 3:
+                            Password += specialChar[rand.Next(0, 10)].ToString(); //10 otherwise 9 won't be included
+                            break;
                     }
                 }
-            }//end of if not special
-            pass_Box.Clear();
-            pass_Box.Text = Password;
 
+            }
 
+            //if no special
+            if (!chkbox_special_Char.Checked)
+            {
+                for (int i = 0; i < length; i++) // loops length of password
+                {
+                    int selectlist = rand.Next(0, 3);
+                    switch (selectlist)
+                    {
+                        //lower
+                        case 0:
+                            Password += lowerCase[rand.Next(0, 26)].ToString();
+                            break;
+
+                        //upper
+                        case 1:
+                            Password += upperCase[rand.Next(0, 26)].ToString();
+                            break;
+
+                        //Number
+                        case 2:
+                            Password += rand.Next(0, 10).ToString(); //10 otherwise 9 won't be included
+                            break;
+                    }
+                }
+            }
+            pass_Box.Clear(); //ensure password is blank when inputting password
+            pass_Box.Text = Password; //posts password into text box
         }
     }
 }
